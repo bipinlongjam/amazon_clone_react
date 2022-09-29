39188cc487925a176d5b419ab9f2080926f1f3b1
bipinlongjam <bipinlongjam11@gmail.com>
2022-09-29 17:56:23 +0530

    second comment

--- src/reducer.js
+++ src/reducer.js
@@ -26,6 +26,11 @@ const reducer = (state, action) => {
                 ...state,
                 basket:newBasket
             }
+        case "SET_USER":
+            return{
+                ...state,
+                user:action.user
+            }
             default:
                 return state;
     }
